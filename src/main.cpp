2eba90de9b6b2c3c74ec9fc69e93f7700aa6ec91
franciscoctavares <francisco.ctavares@protonmail.com>
2025-09-09 16:04:41 +0100

    removed non used include statements from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,4 @@
-#include <iostream>
-
-#include "matrix.h"
-#include "lp/constraint.h"
-#include "lp/lp.h"
-#include "model_reader.h"
-#include "bb_node.h"
-#include "bb_tree.h"
-#include "bb_utils.h"
 #include "cli/cli.h"
-#include "tabulate.hpp"
-
-#include <fstream>
-#include <iostream>
-
-#include "lp/lp_simplifier.h"
-
-using namespace std;
 
 int main(int argc, char** argv) {
     CLI interface(argc, argv);
